7be9926c39d6472bf9223368e9c9b8bf8c048df1
robinsse <srobins@byupathway.edu>
2025-10-06 21:32:18 -0600

    W05 Form Assignment

---
+++ week05/scripts/form.js
@@ -0,0 +1,55 @@
+const today = new Date();
+currentyear.innerHTML = today.getFullYear();
+
+let when = document.querySelector("#when");
+let lastMod = new Date(document.lastModified);
+when.innerHTML = lastMod.toLocaleDateString();
+
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+
+const productList = document.querySelector('#product');
+
+products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    productList.appendChild(option);
+});
+
+let count = getCounter() || 0;
+count++;
+setCounter(count);
+
+function setCounter(count) {
+    localStorage.setItem('count', JSON.stringify(count));
+}
+
+function getCounter() {
+    return JSON.parse(localStorage.getItem('count'));
+}
